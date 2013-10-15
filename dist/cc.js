4e761d287f4b0caa6807cf7755283ba6bd1f64cb
fangwei <fangweifzfj@163.com>
2013-10-15 14:59:58 +0800

    use grunt got the result.

---
+++ dist/cc.js
@@ -0,0 +1,12 @@
+/*! cc - v0.1.0 - 2013-10-15
+* https://github.com/fangwei/cc
+* Copyright (c) 2013 fangwei; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
